0ddc0deef18890a0d5737e8502a0e200f08a3bce
Henrik Lissner <henrik@lissner.net>
2017-12-23 02:29:37 -0500

    Initial commit

---
+++ autoload.el
@@ -0,0 +1,6 @@
+;;; private/hlissner/autoload.el -*- lexical-binding: t; -*-
+
+;;;###autoload (autoload '+hlissner/find-in-dotfiles "private/hlissner/autoload/hlissner" nil t)
+(+default--def-find-in! dotfiles (expand-file-name "~/.dotfiles"))
+;;;###autoload (autoload '+hlissner/browse-dotfiles "private/hlissner/autoload/hlissner" nil t)
+(+default--def-browse-in! dotfiles (expand-file-name "~/.dotfiles"))
---
+++ init.el
@@ -0,0 +1,22 @@
+;;; private/hlissner/init.el -*- lexical-binding: t; -*-
+
+;; I've swapped these keys on my keyboard
+(setq x-super-keysym 'alt
+      x-alt-keysym   'meta
+
+      user-mail-address "henrik@lissner.net"
+      user-full-name    "Henrik Lissner"
+
+      doom-font (font-spec :family "Fira Mono" :size 12)
+      doom-variable-pitch-font (font-spec :family "Fira Sans")
+      doom-unicode-font (font-spec :family "DejaVu Sans Mono")
+      doom-big-font (font-spec :family "Fira Mono" :size 19)
+
+      org-ellipsis "  ")
+
+;; Fira Mono doesn't have italics, so we highlight it instead.
+(add-hook! doom-post-init
+  (set-face-attribute 'italic nil :weight 'ultra-light :foreground "#ffffff"))
+
+(add-hook! doom-big-font-mode
+  (setq +doom-modeline-height (if doom-big-font-mode 37 29)))
