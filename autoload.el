22c33d911ebd8eb7167cd6c4b5487a8f8cdedf95
Henrik Lissner <henrik@lissner.net>
2018-02-18 20:44:28 -0500

    Move autoload.el => autoload/hlissner.el
    I expect to add more files

--- autoload.el
+++
@@ -1,6 +0,0 @@
-;;; private/hlissner/autoload.el -*- lexical-binding: t; -*-
-
-;;;###autoload (autoload '+hlissner/find-in-dotfiles "private/hlissner/autoload/hlissner" nil t)
-(+default--def-find-in!   dotfiles (expand-file-name "~/.dotfiles") +hlissner)
-;;;###autoload (autoload '+hlissner/browse-dotfiles "private/hlissner/autoload/hlissner" nil t)
-(+default--def-browse-in! dotfiles (expand-file-name "~/.dotfiles") +hlissner)
