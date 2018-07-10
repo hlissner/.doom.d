a628fa8c92891103b941b880bc1ee97dd16059ce
Henrik Lissner <henrik@lissner.net>
2018-07-10 12:55:58 +0200

    Sneaky breaky eager-loading of big packages
    This update has two objectives:
    
    1. Make org and magit start up quickly in the future,
    2. And don't noticeably block Emacs while you load org/magit.

--- config.el
+++ config.el
@@ -28,6 +28,23 @@
 
 (setq-hook! 'minibuffer-setup-hook show-trailing-whitespace nil)
 
+;; be sneaky breeky about loading org+magit
+(when EMACS26+
+  (defun auto-require-packages (packages)
+    (let* ((reqs (cl-remove-if #'featurep packages))
+           (req (pop reqs)))
+      (when req
+        ;; (message "Loading %s" req)
+        (require req)
+        (when reqs
+          (run-with-idle-timer 1 nil #'auto-require-packages reqs)))))
+
+  (run-with-idle-timer 1 nil #'auto-require-packages
+                       '(calendar find-func format-spec org-macs org-compat
+                         org-faces org-entities org-list org-pcomplete org-src
+                         org-footnote org-macro ob org org-clock org-agenda
+                         org-capture with-editor git-commit package magit)))
+
 
 ;;
 ;; Host-specific config
