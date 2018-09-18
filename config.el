61277416510432fae63d43ab4b538e70e31c9f45
Henrik Lissner <henrik@lissner.net>
2018-09-18 18:37:54 -0400

    Bind C-{h,j,k,l}
    Since this was removed upstream in Doom

--- config.el
+++ config.el
@@ -65,6 +65,12 @@
 ;;
 
 (map!
+ ;; Easier window navigation
+ :n "C-h"   #'evil-window-left
+ :n "C-j"   #'evil-window-down
+ :n "C-k"   #'evil-window-up
+ :n "C-l"   #'evil-window-right
+
  (:after treemacs-evil
    (:map evil-treemacs-state-map
      "C-h" #'evil-window-left
