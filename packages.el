15799fa55e06a0f763e04da26f193c7f3b01ef96
Henrik Lissner <henrik@lissner.net>
2019-02-25 01:34:05 -0500

    Reformat packages.el & add comments

--- packages.el
+++ packages.el
@@ -1,4 +1,10 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ~/.config/doom/packages.el
 
-(disable-packages! emacs-snippets exec-path-from-shell)
+;; I already use this snippets library (in $DOOMDIR/snippets)
+(package! emacs-snippets :disable t)
+
+;; Only relevant on MacOS. exec-path-from-shell isn't installed on
+;; Linux/Windows. I use bin/doom patch-macos, which renders exec-path-from-shell
+;; redundant.
+(package! exec-path-from-shell :disable t)
