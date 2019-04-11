6fcfedbf0cb44a4f045415e0a8a376b000932dd2
Henrik Lissner <henrik@lissner.net>
2019-04-10 21:54:59 -0400

    Update packages.el; disable which-key

--- packages.el
+++ packages.el
@@ -1,10 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ~/.config/doom/packages.el
 
-;; I already use this snippets library (in $DOOMDIR/snippets)
+;; `emacs-snippets' *is* my personal snippets library (in $DOOMDIR/snippets)
 (package! emacs-snippets :disable t)
 
-;; Only relevant on MacOS. exec-path-from-shell isn't installed on
-;; Linux/Windows. I use bin/doom patch-macos, which renders exec-path-from-shell
-;; redundant.
-(package! exec-path-from-shell :disable t)
+;; I never use it.
+(package! which-key :disable t)
