dfed2abedee664eeae664fdcf0cf7a26e45fce86
Henrik Lissner <henrik@lissner.net>
2019-05-13 18:02:44 -0400

    Disable idle company completion

--- config.el
+++ config.el
@@ -12,7 +12,8 @@
 
       doom-font (font-spec :family "Input Mono Narrow" :size 12)
       doom-variable-pitch-font (font-spec :family "Noto Sans" :size 14)
-      doom-big-font (font-spec :family "Fira Mono" :size 19))
+
+      company-idle-delay nil)
 
 
 ;;
