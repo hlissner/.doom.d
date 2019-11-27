e1dafd7b9a1049c26eb505d6c30197f8af8181fd
Henrik Lissner <henrik@lissner.net>
2019-11-26 20:34:30 -0500

    Refactor recording frame; add open-scratch-frame

--- autoload/hlissner.el
+++ autoload/hlissner.el
@@ -12,11 +12,16 @@
   (interactive)
   (doom-project-browse (expand-file-name "~/.dotfiles")))
 
+
+;;
+;;; Recording frame
+
 (defvar +hlissner--recording-frame nil)
 
 (defun cleanup-recording-frame (frame)
   (when (eq frame +hlissner--recording-frame)
     (with-selected-frame frame
+      (doom-adjust-font-size 2 frame)
       (keycast-mode -1)
       (global-keycast-mode -1)
       (remove-hook 'delete-frame-functions #'cleanup-recording-frame))))
@@ -43,10 +48,56 @@
     (set-window-buffer (frame-root-window +hlissner--recording-frame)
                        (current-buffer)))
   (with-selected-frame +hlissner--recording-frame
+    (doom-adjust-font-size 2 +hlissner--recording-frame)
     (set-frame-size nil 840 300 t)
-    (doom-adjust-font-size 2)
     (unless (frame-visible-p +hlissner--recording-frame)
       (make-frame-visible)
       (redraw-frame)
       (global-keycast-mode +1)
       (add-hook 'delete-frame-functions #'cleanup-recording-frame))))
+
+
+;;
+;;; Scratch frame
+
+(defvar +hlissner--scratch-frame nil)
+
+(defun cleanup-scratch-frame (frame)
+  (when (eq frame +hlissner--scratch-frame)
+    (with-selected-frame frame
+      (setq doom-fallback-buffer-name (frame-parameter frame 'old-fallback-buffer))
+      (remove-hook 'delete-frame-functions #'cleanup-scratch-frame))))
+
+;;;###autoload
+(defun open-scratch-frame (&optional fn)
+  "Opens the org-capture window in a floating frame that cleans itself up once
+you're done. This can be called from an external shell script."
+  (interactive)
+  (let* ((frame-title-format "")
+         (preframe (cl-loop for frame in (frame-list)
+                            if (equal (frame-parameter frame 'name) "scratch")
+                            return frame))
+         (frame (unless preframe
+                  (make-frame `((name . "scratch")
+                                (width . 120)
+                                (height . 24)
+                                (transient . t)
+                                (internal-border-width . 10)
+                                (left-fringe . 0)
+                                (right-fringe . 0)
+                                (undecorated . t)
+                                ,(if IS-LINUX '(display . ":0")))))))
+    (setq +hlissner--scratch-frame (or frame posframe))
+    (select-frame-set-input-focus +hlissner--scratch-frame)
+    (when frame
+      (with-selected-frame frame
+        (if fn
+            (call-interactively fn)
+          (with-current-buffer (switch-to-buffer "*scratch*")
+            ;; (text-scale-set 2)
+            (when (eq major-mode 'fundamental-mode)
+              (emacs-lisp-mode)))
+          (redisplay)
+          (set-frame-parameter frame 'old-fallback-buffer doom-fallback-buffer-name)
+          (setq doom-fallback-buffer-name "*scratch*")
+          (add-hook 'delete-frame-functions #'cleanup-scratch-frame))))))
