fa8506af7f5d15e5335a6ca0af85dc2387f98eb1
Henrik Lissner <henrik@lissner.net>
2020-04-25 00:51:06 -0400

    Minor reformatting / comment revision

--- packages.el
+++ packages.el
@@ -1,5 +1,7 @@
 ;;; ~/.config/doom/packages.el
 
+(package! atomic-chrome)
+
 (package! doom-snippets
   :recipe (:local-repo "~/projects/conf/doom-snippets"
            :files ("*.el" "snippets")))
@@ -9,5 +11,3 @@
 ;; (package! doom-themes
 ;;   :recipe (:local-repo "~/projects/plugins/emacs-doom-themes/"
 ;;            :files ("*.el" "themes/*.el" "extensions/*.el")))
-
-(package! atomic-chrome)
