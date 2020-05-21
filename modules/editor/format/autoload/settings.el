db945087e394b43a0b6ca1bbdde67132fd8b42b2
Henrik Lissner <henrik@lissner.net>
2020-05-21 16:05:12 -0400

    Add new :editor format module for testing
    Powered by raxod502/apheleia

---
+++ modules/editor/format/autoload/settings.el
@@ -0,0 +1,8 @@
+;;; editor/format/autoload/settings.el -*- lexical-binding: t; -*-
+
+;;;###autodef
+(cl-defun set-formatter!
+    (name formatter &key modes filter ok-statuses error-regexp)
+  "Define (or modify) a formatter named NAME."
+  (declare (indent defun))
+  (message "Not implemented yet"))
---
+++ modules/editor/format/packages.el
@@ -0,0 +1,6 @@
+;; -*- no-byte-compile: t; -*-
+;;; editor/format/packages.el
+
+(package! apheleia
+  :recipe (:host github :repo "raxod502/apheleia")
+  :pin "3e342632b834a78f31ead48b94392e00dba1542c")
